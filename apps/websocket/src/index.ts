f51671cdbff81b69235f47d6f734b73751c33b6d
ANU7MADHAV <anumadhavan888@gmail.com>
2024-12-31 03:22:21 +0530

    feat(dev) : implemented redis

--- apps/websocket/src/index.ts
+++ apps/websocket/src/index.ts
@@ -1,11 +1,28 @@
 import express from "express";
 import http from "http";
 import WebSocket from "ws";
+import Redis from "ioredis";
 
 const app = express();
 const server = http.createServer(app);
 
+const redis = new Redis({
+  host: "localhost",
+  port: 6379,
+});
+
+const QUEUE_NAME = "WebSocket_queue";
+
 const wss = new WebSocket.Server({ server });
+
+const publishQueue = async (message: any) => {
+  try {
+    await redis.lpush(QUEUE_NAME, JSON.stringify(message));
+    console.log("Message published to Redis queue:", message);
+  } catch (error) {
+    console.log("error occured pushing into the queue");
+  }
+};
 
 app.get("/", (req, res) => {
   res.send("WebSocket server running...");
@@ -15,7 +32,11 @@ wss.on("connection", (socket) => {
   console.log("WebSocket connection established");
 
   socket.on("message", (message) => {
-    console.log("message", message);
+    console.log("message", message.toString());
+    publishQueue({
+      timeStamp: new Date(),
+      content: message.toString(),
+    });
   });
 
   socket.off("close", () => {
@@ -23,6 +44,12 @@ wss.on("connection", (socket) => {
   });
 });
 
+redis.on("connection", () => {
+  console.log("redis connection established");
+});
+redis.on("error", () => {
+  console.log("error occured during connection");
+});
 server.listen(8080, () => {
   console.log("server running");
 });

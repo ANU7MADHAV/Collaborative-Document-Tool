35f70bc0419efe893c8aa9cb3741672ec2aaed6a
ANU7MADHAV <anumadhavan888@gmail.com>
2024-12-30 21:06:27 +0530

    feat(dev): addes all user routes

--- apps/http-backend/src/routes/v1Routes.ts
+++ apps/http-backend/src/routes/v1Routes.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import userRouter from "./v1/userRoutes";
+import sheetRouter from "./v1/sheetRoutes";
 
 const v1Router = express.Router();
 
 v1Router.use("/v1/user", userRouter);
+v1Router.use("/v1/", sheetRouter);
 
 export default v1Router;
